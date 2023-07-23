b4385b8e2ee0f2aa589436379426691e88b3dedc
Huyson <5705943+lycheejello@users.noreply.github.com>
2023-07-22 21:28:02 -0700

    Dockerfile, file upload

--- app.py
+++ app.py
@@ -12,22 +12,36 @@ m = []
 
 @app.route("/", methods=("GET", "POST"))
 def index():
-    if m == []:
-        m.append(add_message("Here is unstructured deck data: \n\n"))
-        m.append(add_message(read_deck()))
+    #if m == []:
+        #m.append(add_message("Here is unstructured deck data: \n\n"))
+        #m.append(add_message(read_deck()))
 
 
     if request.method == "POST":
-        p = request.form["prompt"]
-        m.append(add_message(p))
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=m,
-            temperature=0.6,
-        )
-        print(response)
-        m.append(response.choices[0].message)
-        return redirect(url_for("index", result=m))
+        if "prompt" in request.form:
+            if m == []:
+                print("no messages")
+                return redirect(url_for("index", result=[add_message("Error: No Deck")]))
+            p = request.form["prompt"]
+            print("pee")
+            print(p)
+            m.append(add_message(p))
+            r = openapi_request(m)
+            m.append(r.choices[0].message)
+            return redirect(url_for("index", result=m))
+        elif "deck" in request.files:
+            f = request.files
+            print(f)
+            d = request.files["deck"]
+            print("reading:")
+            print(d)
+            if d == "":
+                print("Error: empty file")
+                return redirect(url_for("index", result=[add_message("Error: Empty file")]))
+                
+            u = upload_deck(d)
+            m.append(add_message(u))
+            return redirect(url_for("index", result=m))
 
     result = request.args.get("result")
     return render_template("index.html", result=m)
@@ -56,6 +70,27 @@ Names:""".format(
 def console():
     l = openai.Model.list()
     return(l)
+
+def openapi_request(msg):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=msg,
+        temperature=0.6,
+    )
+    #print(response)
+
+    return response
+
+def upload_deck(d):
+    print("uplaoding")
+    with fitz.open("pdf", d.read()) as doc:  # open document
+        text = chr(12).join([page.get_text() for page in doc])
+        #print ("number of pages: %i" % doc.page_count)
+        print (doc.metadata)
+
+    print("read deck:")
+    print(text)
+    return(text)
 
 
 def read_deck():
